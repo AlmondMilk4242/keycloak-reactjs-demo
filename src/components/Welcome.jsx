ede0a9253a7a700a2c6a2be752592da392a11e62
kara <shankara.ncsb@gmail.com>
2023-01-27 19:58:52 +0800

    push

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,12 @@
 import UserService from "../services/UserService";
+import { Button } from "@mui/material";
 
 const Welcome = () => (
   <div className="jumbotron">
     <h1>Hello Anonymous!</h1>
     <p className="lead">Please authenticate yourself!</p>
     <p>
-      <button className="btn btn-lg btn-warning" onClick={() => UserService.doLogin()}>Login</button>
+      <Button className="btn btn-lg btn-warning" onClick={() => UserService.doLogin()}>Login</Button>
     </p>
   </div>
 )
